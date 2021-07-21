99a09d2825625a111969c8893fd64902e92553bf
MicherGit <40839581+TwelvianZeta@users.noreply.github.com>
2021-07-21 18:09:08 -0400

    a bunnch of things

--- src/main/java/zeta/witchlings/Witchlings.java
+++ src/main/java/zeta/witchlings/Witchlings.java
@@ -1,8 +1,8 @@
-package net.fabricmc.example;
+package zeta.witchlings;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class Witchlings implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
